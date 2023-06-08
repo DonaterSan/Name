ef4ad9d6c7180c9ff45bfd5669cdab470dadcb7e
DonaterSan <134006061+DonaterSan@users.noreply.github.com>
2023-06-08 17:35:49 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(20), nullable=False)
+    text = db.Column(db.String(5, 20), nullable=False)
 
     def __repr__(self):
         return '<Article %r>' % self.id
@@ -20,18 +20,17 @@ class Article(db.Model):
 @app.route('/test', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
-        def write_file(data):
+        def write_file(date):
             with open('list.txt', 'a') as f:
-                f.write(data + '\n')
-
+                f.write(str(date))
         text = request.form['text']
-        write_file(text)
+        write_file(text + '\n')
         article = Article(text=text)
 
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/succes')
+            return redirect('/success')
         except:
             return 'Миша всё ху@ня давай по новой'
     else:
@@ -44,15 +43,27 @@ def names():
     return render_template('names_f.html', articles=articles or [])
 
 
+@app.route('/names/<article_id>/del')
+def names_del(article_id):
+    article = Article.query.get_or_404(article_id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/names')
+    except:
+        return "Миша всё ху@ня давай по новой"
+
+
 @app.route('/names/<article_id>')
 def names_item(article_id):
     article = Article.query.get(article_id)
     return render_template('names_item.html', article=article)
 
 
-@app.route('/succes')
+@app.route('/success')
 def succes():
-    return render_template("succes.html")
+    return render_template("success.html")
 
 
 if __name__ == "__main__":
